1b55180a1b3bb9e9eb64683ef72ac662b6d0129c
awitherow <au.witherow@gmail.com>
2017-02-16 01:25:05 +0200

    feat(i18n): add translations for hello page

---
+++ src/locales.js
@@ -0,0 +1,14 @@
+export default {
+  'en': {
+    'brand': {
+      'title': 'OpenRefuge',
+      'subtitle': 'Welcoming refugees with open arms'
+    },
+    'views': {
+      'hello': {
+        'button-seek_help': 'Seek help',
+        'button-provide_support': 'Provide support'
+      }
+    }
+  }
+}
