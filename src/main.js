ec387a675721883d29de68e255e12eb9f75eb036
awitherow <au.witherow@gmail.com>
2017-02-16 01:25:05 +0200

    feat(i18n): add translations for hello page

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueI18n from 'vue-i18n'
+import locales from './locales'
+
+Vue.use(VueI18n)
+
+Vue.config.lang = 'en'
+
+Object.keys(locales).forEach(lang => Vue.locale(lang, locales[lang]))
 
 /* eslint-disable no-new */
 new Vue({
