13a25b1ac04827a5e8bcc595c999269e51d8ade9
awitherow <au.witherow@gmail.com>
2017-02-16 12:02:29 +0200

    chore(Hello): transfer button styles to links

--- src/components/views/Hello.vue
+++ src/components/views/Hello.vue
@@ -4,8 +4,8 @@
       <h1>{{ title }}</h1>
       <h2>{{ subtitle }}</h2>
       <div class="button-container">
-        <router-link to="/refugees">Seek help</router-link>
-        <router-link to="/support">Provide support</router-link>
+        <router-link class="button-link" to="/refugees">Seek help</router-link>
+        <router-link class="button-link" to="/support">Provide support</router-link>
       </div>
     </div>
   </div>
@@ -26,12 +26,15 @@ export default {
   text-align: center;
 }
 
-button {
+.button-link {
   border: 1px solid black;
   padding: 10px;
   background: none;
   text-transform: uppercase;
   min-width: 100px;
   margin: 5px;
+  text-decoration: none;
+  color: black;
+  font-size: 12px;
 }
 </style>
