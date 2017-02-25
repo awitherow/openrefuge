4a60dd2558a9dcbb17f5e9a8711105846139e0f1
awitherow <au.witherow@gmail.com>
2017-02-25 16:34:40 +0100

    chore(server): enable reading of gzipped js

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const app = express()
 app.use(compression())
 
 app.use(express.static('dist'))
+app.get('*.js', function (req, res, next) {
+  req.url = req.url + '.gz'
+  res.set('Content-Encoding', 'gzip')
+  next()
+})
 
 app.set('port', (process.env.PORT || 3333))
 app.listen(app.get('port'), () =>
