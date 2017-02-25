57f5634d1579e4b0e7ed0d8c5959aeb9a283ff80
awitherow <au.witherow@gmail.com>
2017-02-25 21:51:50 +0100

    chore(style): refuge button green

--- src/components/views/Hello.vue
+++ src/components/views/Hello.vue
@@ -6,7 +6,7 @@
       <h2>{{ $t("brand.subtitle") }}</h2>
 
       <div class="button-container">
-        <router-link class="button-link" to="/refugees">
+        <router-link class="button-link green" to="/refugees">
           {{ $t("views.hello.button-seek_help") }}
         </router-link>
         <router-link class="button-link" to="/support">
@@ -31,14 +31,20 @@ export default {
 }
 
 .button-link {
-  border: 1px solid black;
+  border: 1px solid #444;
   padding: 10px;
   background: none;
   text-transform: uppercase;
   min-width: 100px;
   margin: 5px;
   text-decoration: none;
-  color: black;
+  color: #444;
   font-size: 12px;
+}
+
+.button-link.green {
+  border-color: #3fb618;
+  background: #3fb618;
+  color: white;
 }
 </style>
