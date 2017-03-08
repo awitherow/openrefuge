031841596f2f5ae8c4c945029284a1010b959574
awitherow <au.witherow@gmail.com>
2017-03-08 19:53:53 +0100

    chore(About): add simple styling

--- src/components/views/About.vue
+++ src/components/views/About.vue
@@ -1,10 +1,13 @@
 <template>
   <div id="about" class="container">
+    <h1>Our Mission</h1>
     <div class="mission">
       <p v-for="paragraph in paragraphs">{{ paragraph }}</p>
     </div>
+
+    <h1>Meet the staff</h1>
     <div class="workers">
-      <div v-for="worker in workers">
+      <div v-for="worker in workers" class="worker">
         <img :src="worker.photo" />
         <h2>{{ worker.name }}</h2>
         <p>{{ worker.description }}</p>
@@ -23,12 +26,12 @@ export default {
         {
           photo: 'https://placeholdit.imgix.net/~text?txtsize=33&txt=350%C3%97150&w=500&h=500',
           name: 'Austin',
-          description: 'Technical "executive" for OpenRefuge, as well as Project Manager for our entire cause.'
+          description: 'Technical lead for development and planning at OpenRefuge, as well as Project Manager for our entire cause.'
         },
         {
           photo: 'https://placeholdit.imgix.net/~text?txtsize=33&txt=350%C3%97150&w=500&h=500',
           name: 'Jeff',
-          description: 'Mission leader for OpenRefuge, inviting and inspiring people to help join hands to make the world a better place'
+          description: 'Mission leader at OpenRefuge, inviting and inspiring people to help join hands to make the world a better place'
         },
         {
           photo: 'https://placeholdit.imgix.net/~text?txtsize=33&txt=350%C3%97150&w=500&h=500',
@@ -43,6 +46,28 @@ export default {
 
 <style scoped>
 #about {
+  text-align: justify;
+}
+
+.workers {
+  display: flex;
+}
+
+.worker {
+  margin: 25px;
+}
+
+.worker img {
+    border-radius: 125px;
+    height: 250px;
+    width: 250px;
+}
+
+.worker h2 {
+  text-align: center;
+}
+
+.worker p {
   text-align: justify;
 }
 
