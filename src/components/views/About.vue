335d95b0e7d491176c9bd98221594cefb42bd855
awitherow <au.witherow@gmail.com>
2017-03-08 19:37:36 +0100

    chore(About): add worker scaffolding

--- src/components/views/About.vue
+++ src/components/views/About.vue
@@ -1,6 +1,15 @@
 <template>
   <div id="about" class="container">
-    <p v-for="paragraph in paragraphs">{{ paragraph }}</p>
+    <div class="mission">
+      <p v-for="paragraph in paragraphs">{{ paragraph }}</p>
+    </div>
+    <div class="workers">
+      <div v-for="worker in workers">
+        <img :src="worker.photo" />
+        <h2>{{ worker.name }}</h2>
+        <p>{{ worker.description }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,7 +18,24 @@ export default {
   name: 'hello',
   data () {
     return {
-      paragraphs: this.$t('views.about.paragraphs')
+      paragraphs: this.$t('views.about.paragraphs'),
+      workers: [
+        {
+          photo: 'https://placeholdit.imgix.net/~text?txtsize=33&txt=350%C3%97150&w=500&h=500',
+          name: 'Austin',
+          description: 'Technical "executive" for OpenRefuge, as well as Project Manager for our entire cause.'
+        },
+        {
+          photo: 'https://placeholdit.imgix.net/~text?txtsize=33&txt=350%C3%97150&w=500&h=500',
+          name: 'Jeff',
+          description: 'Mission leader for OpenRefuge, inviting and inspiring people to help join hands to make the world a better place'
+        },
+        {
+          photo: 'https://placeholdit.imgix.net/~text?txtsize=33&txt=350%C3%97150&w=500&h=500',
+          name: 'Adam',
+          description: 'Philosopher and writer at OpenRefuge who also helps with business management.'
+        }
+      ]
     }
   }
 }
