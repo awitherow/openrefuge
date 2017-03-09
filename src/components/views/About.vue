14d13b245891a050359edf15174a22720d459cc4
awitherow <au.witherow@gmail.com>
2017-03-09 23:22:38 +0100

    chore(About): add local teams

--- src/components/views/About.vue
+++ src/components/views/About.vue
@@ -6,11 +6,28 @@
     </div>
 
     <h1>Meet the staff</h1>
+    <h2>Administraton</h2>
     <div class="workers">
-      <div v-for="worker in workers" class="worker">
-        <img :src="worker.photo" />
-        <h2>{{ worker.name }}</h2>
-        <p>{{ worker.description }}</p>
+      <div v-for="admin in admins" class="worker">
+        <img :src="admin.photo" />
+        <h2>{{ admin.name }}</h2>
+        <p>{{ admin.description }}</p>
+      </div>
+    </div>
+    <h2>Team Athens, Greece</h2>
+    <div class="workers">
+      <div v-for="greekers in greeceTeam" class="worker">
+        <img :src="greekers.photo" />
+        <h2>{{ greekers.name }}</h2>
+        <p>{{ greekers.description }}</p>
+      </div>
+    </div>
+    <h2>Team Hamburg, Germany</h2>
+    <div class="workers">
+      <div v-for="hamburgers in hamburgTeam" class="worker">
+        <img :src="hamburgers.photo" />
+        <h2>{{ hamburgers.name }}</h2>
+        <p>{{ hamburgers.description }}</p>
       </div>
     </div>
   </div>
@@ -22,21 +39,40 @@ export default {
   data () {
     return {
       paragraphs: this.$t('views.about.paragraphs'),
-      workers: [
+      admins: [
         {
-          photo: 'http://i.imgur.com/DC4nEAQ.jpg',
+          photo: 'http://i.imgur.com/T9C2WEJ.jpg',
           name: 'Austin',
           description: 'Technical lead for development and planning at OpenRefuge, as well as Project Manager for our entire cause.'
         },
         {
-          photo: 'http://i.imgur.com/bW8aGkc.jpg',
+          photo: 'http://i.imgur.com/luKz0jq.jpg',
           name: 'Jeff',
           description: 'Mission leader at OpenRefuge, inviting and inspiring people to help join hands to make the world a better place'
         },
         {
-          photo: 'http://i.imgur.com/FxzSDb0.jpg',
+          photo: 'http://i.imgur.com/cURePXv.jpg',
           name: 'Adam',
           description: 'Philosopher and writer at OpenRefuge who also helps with business management.'
+        }
+      ],
+      greeceTeam: [
+        {
+          photo: 'http://i.imgur.com/bK2o0mw.jpg',
+          name: 'Moha',
+          description: 'A refugee from Syria pursuing his doctorate in Law, working within the refugee and migrant community in Athens.'
+        },
+        {
+          photo: 'http://i.imgur.com/leGPG4Q.jpg',
+          name: 'Rita',
+          description: 'Helps in social support, case study, and providing clothing, food distribution and shelter for unofficial and homeless refugees.'
+        }
+      ],
+      hamburgTeam: [
+        {
+          photo: 'http://i.imgur.com/pNtRO0g.jpg',
+          name: 'Maja',
+          description: 'Works directly with refugees in the community of Hamburg, assisting them with cultural integration.'
         }
       ]
     }
