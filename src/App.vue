a01eeebcb617d0b8e55791f5b5291e82d2b7223a
awitherow <au.witherow@gmail.com>
2017-03-10 12:17:05 +0100

    WIP

--- src/App.vue
+++ src/App.vue
@@ -20,6 +20,22 @@ export default {
 <style>
 html {
   box-sizing: border-box;
+  background: url(http://i.imgur.com/sSezI4R.jpg) no-repeat center center fixed;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+}
+
+html:before {
+  content: '';
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background-image: linear-gradient(to bottom right,#fff,#fff);
+  opacity: .9;
 }
 
 *, *:before, *:after {
