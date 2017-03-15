cb2dcca79ba4f63fbde47caf984f0f54b6131a33
awitherow <au.witherow@gmail.com>
2017-03-15 19:16:19 +0100

    WIP

--- server.js
+++ server.js
@@ -1,27 +1,23 @@
 const express = require('express')
-const compression = require('compression')
-const history = require('connect-history-api-fallback')
-
-const staticMiddleWare = express.static('dist')
 
 const app = express()
-app.use(compression())
-app.use(staticMiddleWare)
-app.use(history())
-app.use(staticMiddleWare)
 
-app.use(express.static('dist'))
-app.get('*.js', function (req, res, next) {
-  req.url = req.url + '.gz'
-  res.set('Content-Encoding', 'gzip')
-  next()
-})
+if (process.env.APP_ENV === 'PROD') {
+  const compression = require('compression')
+  const history = require('connect-history-api-fallback')
+  const staticMiddleWare = express.static('dist')
 
-app.get('*.js', function (req, res, next) {
-  req.url = req.url + '.gz'
-  res.set('Content-Encoding', 'gzip')
-  next()
-})
+  app.use(compression())
+  app.use(staticMiddleWare)
+  app.use(history())
+  app.use(staticMiddleWare)
+
+  app.get('*.js', function (req, res, next) {
+    req.url = req.url + '.gz'
+    res.set('Content-Encoding', 'gzip')
+    next()
+  })
+}
 
 app.set('port', (process.env.PORT || 3333))
 app.listen(app.get('port'), () =>
