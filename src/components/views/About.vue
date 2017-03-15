850151b2a878cc248e2d855747f4ff54ad2904e9
awitherow <au.witherow@gmail.com>
2017-03-15 20:26:21 +0100

    chore(About): update our team info

--- src/components/views/About.vue
+++ src/components/views/About.vue
@@ -5,29 +5,12 @@
       <p v-for="paragraph in paragraphs" v-html="paragraph" />
     </div>
 
-    <h1>Meet the staff</h1>
-    <h2>Team Athens, Greece</h2>
-    <div class="workers">
-      <div v-for="greekers in greeceTeam" class="worker">
-        <img :src="greekers.photo" />
-        <h3>{{ greekers.name }}</h3>
-        <p>{{ greekers.description }}</p>
-      </div>
-    </div>
-    <h2>Team Hamburg, Germany</h2>
-    <div class="workers">
-      <div v-for="hamburgers in hamburgTeam" class="worker">
-        <img :src="hamburgers.photo" />
-        <h3>{{ hamburgers.name }}</h3>
-        <p>{{ hamburgers.description }}</p>
-      </div>
-    </div>
-    <h2>Administraton</h2>
-    <div class="workers">
-      <div v-for="admin in admins" class="worker">
-        <img :src="admin.photo" />
-        <h3>{{ admin.name }}</h3>
-        <p v-html="admin.description" />
+    <h1>OpenRefuge Contributors</h1>
+    <div class="people">
+      <div v-for="person in people" class="person">
+        <img :src="person.photo" />
+        <h3>{{ person.name }}</h3>
+        <p v-html="person.description"/>
       </div>
     </div>
   </div>
@@ -39,16 +22,26 @@ export default {
   data () {
     return {
       paragraphs: this.$t('views.about.paragraphs'),
-      admins: [
+      people: [
         {
-          photo: 'static/austin.jpg',
-          name: 'Austin',
-          description: 'Technical lead for development and planning at OpenRefuge, as well as Project Manager for our entire cause.'
+          photo: 'static/moha.jpg',
+          name: 'Moha',
+          description: 'A refugee from Syria who studied English Literature in Syria, works now in Athens at Médecins Sans Frontières (Doctors Without Borders), and also volunteers in the refugee squats, as well as in Port Periaus.'
+        },
+        {
+          photo: 'static/maja.jpg',
+          name: 'Maja',
+          description: 'Works directly with refugees in the community of Hamburg, assisting with cultural integration.'
         },
         {
           photo: 'static/jeff-skittles.jpg',
           name: 'Jeff',
           description: 'Mission leader at OpenRefuge, inviting and inspiring people to help join hands to make the world a better place.'
+        },
+        {
+          photo: 'static/austin.jpg',
+          name: 'Austin',
+          description: 'Technical lead for development and planning at OpenRefuge, as well as Project Manager for our entire cause.'
         },
         {
           photo: 'static/adam.jpg',
@@ -59,20 +52,6 @@ export default {
           photo: 'static/placeholder-person.png',
           name: 'Social Media Expert',
           description: 'We are interested in someone who is a social media expert to do part time work weekly to help lead our social campaigns. <a href="mailto:contactopenrefuge@gmail.com?Subject=Social%20Media%20Application%20help." target="_top">Send us an email application</a>!'
-        }
-      ],
-      greeceTeam: [
-        {
-          photo: 'static/moha.jpg',
-          name: 'Moha',
-          description: 'A refugee from Syria who studied English Literature in Syria, works now in Athens at Médecins Sans Frontières (Doctors Without Borders), and also volunteers in the refugee squats, as well as in Port Periaus.'
-        }
-      ],
-      hamburgTeam: [
-        {
-          photo: 'static/maja.jpg',
-          name: 'Maja',
-          description: 'Works directly with refugees in the community of Hamburg, assisting with cultural integration.'
         }
       ]
     }
@@ -85,7 +64,7 @@ export default {
   text-align: justify;
 }
 
-.workers {
+.people {
   display: flex;
   align-items: center;
   justify-content: center;
@@ -93,12 +72,12 @@ export default {
   text-align: center;
 }
 
-.worker {
+.person {
   margin: 15px;
   max-width: 300px;
 }
 
-.worker img {
+.person img {
   border-radius: 100px;
   height: 200px;
   width: 200px;
@@ -108,7 +87,7 @@ h2 {
   text-align: center;
 }
 
-.worker p {
+.person p {
   text-align: left;
 }
 
@@ -129,13 +108,13 @@ h2 {
     padding-top: 0;
   }
 
-  .workers {
+  .people {
     flex-direction: row;
     align-items: flex-start;
     flex-wrap: wrap;
   }
 
-  .worker img {
+  .person img {
     border-radius: 87.5px;
     height: 175px;
     width: 175px;
@@ -148,19 +127,19 @@ h2 {
     max-width: 1000px;
   }
 
-  .workers {
+  .people {
     flex-direction: row;
   }
 
-  .worker {
+  .person {
     margin: 25px;
-    max-width: 350px;
+    max-width: 250px;
   }
 
-  .worker img {
-    border-radius: 125px;
-    height: 250px;
-    width: 250px;
+  .person img {
+    border-radius: 100px;
+    height: 200px;
+    width: 200px;
   }
 }
 </style>
