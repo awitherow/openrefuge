0e7d14b19a288f27ba36d796d5a1d7d26f1663bd
awitherow <au.witherow@gmail.com>
2017-03-15 19:28:55 +0100

    fix(server): add history fallback

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require('express')
 const compression = require('compression')
+const history = require('connect-history-api-fallback')
+
+const staticMiddleWare = express.static('dist')
 
 const app = express()
 app.use(compression())
+app.use(staticMiddleWare)
+app.use(history())
+app.use(staticMiddleWare)
 
 app.use(express.static('dist'))
+app.get('*.js', function (req, res, next) {
+  req.url = req.url + '.gz'
+  res.set('Content-Encoding', 'gzip')
+  next()
+})
+
 app.get('*.js', function (req, res, next) {
   req.url = req.url + '.gz'
   res.set('Content-Encoding', 'gzip')
