925040e87877afd29d880efc50c4632aee1d5a3d
awitherow <au.witherow@gmail.com>
2017-04-12 17:03:05 +0200

    chore(maps): screw it expose the api key

--- src/main.js
+++ src/main.js
@@ -5,19 +5,10 @@ import VueI18n from 'vue-i18n'
 import locales from './locales'
 import * as VueGoogleMaps from 'vue2-google-maps'
 
-let GOOGLE_MAPS_TOKEN = ''
-if (process.env.NODE_ENV === 'development') {
-  const keys = require('./config/keys.json')
-  GOOGLE_MAPS_TOKEN = keys.GOOGLE_MAPS_TOKEN
-} else {
-  GOOGLE_MAPS_TOKEN = process.env.GOOGLE_MAPS_TOKEN
-  console.log(process.env)
-}
-
 Vue.use(VueGoogleMaps, {
   installComponents: true,
   load: {
-    key: GOOGLE_MAPS_TOKEN
+    key: 'AIzaSyCRn24HTFi98D0b3nztP9g5tZeaPQ4k06g'
   }
 })
 
