72500c540e57afd7a476db7ac9d7ad17e692111d
awitherow <au.witherow@gmail.com>
2017-04-12 15:16:54 +0200

    chore(deps): install and get google maps package working

--- src/main.js
+++ src/main.js
@@ -3,6 +3,15 @@ import App from './App'
 import router from './router'
 import VueI18n from 'vue-i18n'
 import locales from './locales'
+import * as VueGoogleMaps from 'vue2-google-maps'
+import keys from './config/keys.json'
+
+Vue.use(VueGoogleMaps, {
+  installComponents: true,
+  load: {
+    key: keys.GOOGLE_MAPS_TOKEN || process.env.GOOGLE_MAPS_TOKEN
+  }
+})
 
 Vue.use(VueI18n)
 
