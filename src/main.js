cbe026b6d8be0fe66a49c0a0bf720783a7c10fdd
awitherow <au.witherow@gmail.com>
2017-04-12 16:49:37 +0200

    chore(keys): better fetching of GMAPS key

--- src/main.js
+++ src/main.js
@@ -4,14 +4,20 @@ import router from './router'
 import VueI18n from 'vue-i18n'
 import locales from './locales'
 import * as VueGoogleMaps from 'vue2-google-maps'
-import keys from './config/keys.json'
+
+let GOOGLE_MAPS_TOKEN = ''
+if (process.env.NODE_ENV === 'development') {
+  const keys = require('./config/keys.json')
+  GOOGLE_MAPS_TOKEN = keys.GOOGLE_MAPS_TOKEN
+} else {
+  GOOGLE_MAPS_TOKEN = process.env.GOOGLE_MAPS_TOKEN
+  console.log(process.env)
+}
 
 Vue.use(VueGoogleMaps, {
   installComponents: true,
   load: {
-    // key will be gotten from config json keys file, which you can copy from the example.
-    // on production, it will get the environment variable set on heroku!
-    key: keys.GOOGLE_MAPS_TOKEN || process.env.GOOGLE_MAPS_TOKEN
+    key: GOOGLE_MAPS_TOKEN
   }
 })
 
