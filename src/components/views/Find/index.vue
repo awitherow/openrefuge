a0ee196edcf333a3a59d070ed30b5d0fdcf8a064
awitherow <au.witherow@gmail.com>
2017-05-02 15:02:23 +0200

    chore(diakonie): get them on the map

--- src/components/views/Find/index.vue
+++ src/components/views/Find/index.vue
@@ -1,17 +1,7 @@
 <template>
-  <div id="find-help"
-       class="container--full">
-
-    <div class="WIP">
-      <p>Currently a work in progress, stay tuned! Check out <a target="_blank"
-           href="https://github.com/openrefuge/openrefuge">our github</a> if you wanna help with development ;)</p>
-    </div>
-
-    <gmap-map :options="{styles: styles}"
-              style="height: 100%; width: 100%; position: absolute; top: 0; left: 0;"
-              :center="{lat: 37.99, lng: 23.72 }"
-              :zoom="12">
-
+  <div id="find-help" class="container--full">
+    <gmap-map :options="{styles: styles}" style="height: 100%; width: 100%; position: absolute; top: 0; left: 0;" :center="markers[0].position" :zoom="12">
+      <gmap-marker :key="index" v-for="(m, index) in markers" :position="m.position" :clickable="true" @click="showPlaceModal(m.id)"></gmap-marker>
     </gmap-map>
   </div>
 </template>
@@ -21,7 +11,22 @@ export default {
   name: 'find-help',
   data() {
     return {
-      styles: require('./styles.json')
+      styles: require('./styles.json'),
+      center: { lat: 10.0, lng: 10.0 },
+      markers: [{
+        id: 'DE01',
+        name: 'Diakonie-Hilfswerk Hamburg',
+        description: 'Im Mai 2015 startete das Diakonische Hilfswerk das Projekt Flüchtlingslotsen. Das Projekt unterstützt durch geschulte ehrenamtlich Tätige nach Hamburg geflüchtete und migrierte Menschen. Die Lotsen begleiten Hilfesuchende zu Ämtern- und Behörden und anderen Einrichtungen. Auch längerfristige Unterstützung in Form von Patenschaften für Geflüchtete wird vermittelt. Zur Unterstützung der ehrenamtlichen Arbeit von Lotsinnen und Lotsen bietet das Projekt monatlich entweder Fortbildungen zu relevanten Themen oder es finden Treffen zum Austausch mit kollegialer Fallberatung statt.',
+        tags: ['volunteer'],
+        contact: 'Fluechtlingslotsen@diakonie-hamburg.de',
+        site: 'diakonie-hamburg.de',
+        position: { lat: 53.5479505, lng: 9.9347988 }
+      }]
+    }
+  },
+  methods: {
+    showPlaceModal(id) {
+      console.log(id)
     }
   }
 }
