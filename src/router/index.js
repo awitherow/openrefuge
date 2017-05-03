515d908f757e2e46367773c50c1dd7805ef1ba5c
awitherow <au.witherow@gmail.com>
2017-05-03 18:41:04 +0200

    chore(deployment): use 200.html

--- src/router/index.js
+++ src/router/index.js
@@ -1,35 +1,35 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+import Vue from 'vue';
+import Router from 'vue-router';
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
-  mode: 'history',
-  routes: [
-    {
-      path: '/',
-      name: 'hello',
-      component: require('components/views/Hello/')
-    },
-    {
-      path: '/find-help',
-      name: 'find-help',
-      component: require('components/views/Find/')
-    },
-    {
-      path: '/get-involved',
-      name: 'get-involved',
-      component: require('components/views/Involve')
-    },
-    {
-      path: '/about',
-      name: 'About',
-      component: require('components/views/About')
-    },
-    {
-      path: '*',
-      name: '404',
-      component: require('components/views/Hello')
-    }
-  ]
-})
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            name: 'hello',
+            component: require('components/views/Hello/'),
+        },
+        {
+            path: '/find-help',
+            name: 'find-help',
+            component: require('components/views/Find/'),
+        },
+        {
+            path: '/get-involved',
+            name: 'get-involved',
+            component: require('components/views/Involve'),
+        },
+        {
+            path: '/about',
+            name: 'About',
+            component: require('components/views/About'),
+        },
+        {
+            path: '*',
+            name: '404',
+            component: require('components/views/Hello'),
+        },
+    ],
+});
