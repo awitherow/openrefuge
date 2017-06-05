52f80a960cef61194d1f544aa9233578dd6694e2
awitherow <au.witherow@gmail.com>
2017-06-05 12:28:56 +0200

    feat(alert): add basic alert area

--- src/components/views/Hello/index.vue
+++ src/components/views/Hello/index.vue
@@ -1,6 +1,5 @@
 <template>
-  <div id="hello"
-       class="container">
+  <div id="hello" class="container">
 
     <div class="introduction">
       <h1>{{ $t("brand.title") }}</h1>
@@ -8,12 +7,10 @@
       <p v-html="$t('brand.description')" />
 
       <div class="button-container">
-        <router-link class="button green"
-                     to="/find-help">
+        <router-link class="button green" to="/find-help">
           {{ $t("views.hello.button-find_help") }}
         </router-link>
-        <router-link class="button"
-                     to="/get-involved">
+        <router-link class="button" to="/get-involved">
           {{ $t("views.hello.button-get_involved") }}
         </router-link>
       </div>
@@ -22,19 +19,14 @@
     <div class="project-showcase">
       <h2>Featured Projects</h2>
       <div class="projects">
-        <div v-for="project in projects"
-             class="project">
+        <div v-for="project in projects" class="project">
           <h3>{{ project.name }}</h3>
           <div class="project-content">
             <img :src="project.img" />
             <div class="project-direction">
-              <p v-for="p in project.description"
-                 v-html="p" />
+              <p v-for="p in project.description" v-html="p" />
               <div class="button-container">
-                <a v-for="button in project.buttons"
-                   target="_blank"
-                   :href="button.link"
-                   :class="button.class">{{ button.text }}</a>
+                <a v-for="button in project.buttons" target="_blank" :href="button.link" :class="button.class">{{ button.text }}</a>
               </div>
             </div>
           </div>
@@ -80,23 +72,6 @@ export default {
 
 .button-container .button {
   margin-right: 10px;
-}
-
-.button {
-  border: 1px solid #444;
-  padding: 10px;
-  background: none;
-  text-transform: uppercase;
-  min-width: 100px;
-  text-decoration: none;
-  color: #444;
-  font-size: 12px;
-}
-
-.button.green {
-  border-color: #0C8300;
-  background: #3fb618;
-  color: white;
 }
 
 .project-showcase {
