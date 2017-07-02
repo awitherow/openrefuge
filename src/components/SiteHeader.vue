428fc4208836f8338786ebc94359342b5e122441
awitherow <au.witherow@gmail.com>
2017-07-02 19:01:12 +0200

    feat(header): add social media icons

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -2,12 +2,19 @@
   <div class="header">
     <div class="container">
       <router-link exact to="/">
-        <img src="static/logo.png" class="logo" alt="OpenRefuge logo" />
+        <img src="static/logo.png" class="logo" alt="OpenRefuge logo"></img>
       </router-link>
       <nav>
         <ul class="site-links">
-          <li v-for="item in linkList">
+          <li v-bind:key="item.to" v-for="item in linkList">
             <router-link exact :to="item.to">{{ item.text }}</router-link>
+          </li>
+        </ul>
+        <ul class="social-media">
+          <li v-bind:key="media.to" v-for="media in socialMediaSites">
+            <a :href="media.to" target="_blank">
+              <i :class="getIcon(media.site)" aria-hidden="true"></i>
+            </a>
           </li>
         </ul>
       </nav>
@@ -20,6 +27,24 @@ export default {
   name: 'SiteHeader',
   data() {
     return {
+      socialMediaSites: [
+        {
+          site: 'medium',
+          to: 'https://medium.com/openrefuge'
+        },
+        {
+          site: 'facebook',
+          to: 'https://www.facebook.com/OpenRefuge/'
+        },
+        {
+          site: 'instagram',
+          to: 'https://www.instagram.com/openrefuge/'
+        },
+        {
+          site: 'twitter',
+          to: 'https://twitter.com/openrefuge'
+        }
+      ],
       linkList: [
         {
           text: this.$t('elements.SiteHeader.linkList.home'),
@@ -38,6 +63,11 @@ export default {
           to: '/get-involved'
         }
       ]
+    }
+  },
+  methods: {
+    getIcon(icon) {
+      return `fa fa-${icon}`
     }
   }
 }
@@ -65,19 +95,19 @@ export default {
   margin-right: 10px;
 }
 
-.site-links {
+ul {
   list-style: none;
   padding: 0;
   margin: 0;
 }
 
-.site-links li {
+ul li {
   display: inline-block;
   padding: 0 8px;
   font-size: 14px;
 }
 
-.site-links li a {
+ul li a {
   color: black;
   text-decoration: none;
 }
@@ -88,9 +118,37 @@ export default {
   text-decoration: underline;
 }
 
+.social-media {
+  display: none;
+}
+
 @media(min-width: 425px) {
   .site-links li {
     font-size: 16px;
+  }
+}
+
+@media(min-width: 768px) {
+  nav {
+    display: flex;
+    width: 100%;
+  }
+
+  .site-links {
+    margin-right: auto;
+  }
+
+  .social-media {
+    display: block;
+  }
+
+  .social-media a {
+    color: #3da13e;
+    font-size: 16px;
+  }
+
+  .social-media a:hover {
+    color: #8fcee4;
   }
 }
 </style>
